59b40be1182121507d6a73db32f5ed3a7f42ef04
bruno foggia <brunnofoggia@gmail.com>
2024-03-05 12:43:04 -0300

    feat(config.mixin): new methods and abstractions

--- src/domain/worker/mixins/system/retry.mixin.ts
+++ src/domain/worker/mixins/system/retry.mixin.ts
@@ -1,6 +1,16 @@
 import { StageStructureProperties } from '../../../../interfaces/stageParts.interface';
 
 export abstract class RetryMixin {
+    getRetryInterval(options: any = {}) {
+        return (
+            options.retryInterval ||
+            this.stageConfig.config.retryInterval ||
+            this.moduleConfig.config.retryInterval ||
+            this.project._config.retryInterval ||
+            5
+        );
+    }
+
     getRetryLimit() {
         return this.stageConfig.config.retryLimit || this.moduleConfig.config.retryLimit || this.project._config.retryLimit || 3;
     }
@@ -9,11 +19,19 @@ export abstract class RetryMixin {
         return (this.stageExecution.error?.length || 0) + +increaseByOne;
     }
 
-    isLastAttempt() {
-        const attempt = this.getRetryAttempt();
+    isLastAttempt(beforeUpdate = false) {
+        const attempt = this.getRetryAttempt(beforeUpdate);
         const limit = this.getRetryLimit();
 
         return attempt >= limit;
+    }
+
+    shouldRetry(beforeUpdate = false) {
+        const isLastAttempt = this.isLastAttempt(beforeUpdate);
+        const retryDeactivated = this.stageConfig.config.retry === 0 || this.stageConfig.config.retry === false;
+
+        if (retryDeactivated || isLastAttempt) return false;
+        return true;
     }
 }
 
