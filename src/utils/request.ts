aae1298ba786bfd10b8c197f3e39b48c40f4a3d2
bruno foggia <brunnofoggia@gmail.com>
2025-06-26 15:20:51 -0300

    chore(m0reqs): added info

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -9,7 +9,16 @@ export function m0RequestErrorHandler(error: any, data = null, shouldThrow = tru
     m0RequestLog('error', `status: ${error.code}`, `message: ${error.message}`);
     if (error.status) {
         const _path = (error.request?.path || '').split('/').slice(0, 4).join('/');
-        m0RequestLog('details', `path: ${_path}`, `status: ${error.status}`, `message: ${error.message}`);
+        m0RequestLog('details');
+
+        console.log(
+            `host: ${error.request?.protocol}${error.request?.host}:${error.request?.port || '80'}\n`,
+            `basepath: ${_path}\n`,
+            `fullpath: ${error.request?.path}\n`,
+            `method: ${error.request?.method}\n`,
+            `status: ${error.status}\n`,
+            `message: ${error.message}`,
+        );
         if (data) m0RequestLog('data', data);
 
         const status = +error.status;
